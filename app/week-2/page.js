044632fcf0e734696c1f6cd2b921e41e4d608d93
Shub4114 <shubhammadhav.patil@edu.sait.ca>
2024-01-27 13:53:15 -0700

    Week 2 Final Assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+  return (
+    <main>
+        <h1 className="text-4xl font-extrabold mb-8">Shopping List</h1>
+    <div>
+      <StudentInfo />
+     </div> 
+     </main>
+  );
+};
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function StudentInfo() {
+  return (
+    
+      
+      <div>
+        <p>Shubham Patil</p>
+        <Link href="https://github.com/Shub4114/cprg306-assignments.git" >GitHub Repository link</Link>
+      </div>
+    
+  );
+};
